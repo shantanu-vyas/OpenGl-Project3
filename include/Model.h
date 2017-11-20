e39a84c2de82c41d85d0fe2c435ab8993cda7e1a
Shantanu Vyas <shenty@localhost.localdomain>
2017-11-20 15:03:09 -0500

    added makesphere to header

--- include/Model.h
+++ include/Model.h
@@ -32,6 +32,7 @@ void scaleZModel(Model* model, const int* const num_vertices, float factor);
 void deepCopyModel(Model* ret, const Model* const model, const int* const num_vertices);
 void flattenModelList(Model** list, Vec4** v, Vec4** c, int* nv, int* nm);
 void makeCube(Model* cube);
+void makeSphere(Model* sphere);
 void setColor(Model* model, const Vec4* const color);
 
 
